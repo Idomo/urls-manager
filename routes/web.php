e6834ee3c0ac236d8daa04510223f34739b833c6
Idomo <Idomo@users.noreply.github.com>
2023-05-27 18:49:58 +0300

    Convert dashboard to urls list page
    Make sure to redirect to it as the home page when the user is authenticated.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UrlController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,17 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function(){
+    return Auth::check() ? redirect('urls') : view('welcome');
+})->name('/');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('/urls', UrlController::class)
+    ->middleware(['auth', 'verified'])->names('urls');
 
-Route::resource('/urls', UrlController::class);
-
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function(){
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
