b4a3ccd8e29f7d133aad1131f597e1908497dc27
Idomo <Idomo@users.noreply.github.com>
2023-05-30 15:50:53 +0300

    Allow generating shortened url path from UI

--- resources/views/components/text-input-left-addon.blade.php
+++ resources/views/components/text-input-left-addon.blade.php
@@ -8,4 +8,6 @@
         </span>
     </div>
     <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm flex-shrink flex-grow flex-auto leading-normal w-px flex-1 rounded rounded-l-none px-3 relative']) !!}>
+
+    {{ $slot }}
 </div>
--- resources/views/urls/create-form.blade.php
+++ resources/views/urls/create-form.blade.php
@@ -12,10 +12,25 @@
             </div>
 
             <!-- Shortened URL -->
-            <div class="mt-4">
+            <div class="mt-4"
+                 x-data="{shortened: '{{ old('shortened') }}', async generateShortened(){this.shortened = (await (await fetch('/api/urls/generate')).json()).shortened}}">
                 <x-input-label for="shortened" :value="__('Shortened URL')" class="mb-1"/>
                 <x-text-input-left-addon :addonValue="url('/') . '/'" id="shortened" class="block w-full" type="text"
-                                         name="shortened" :value="old('shortened')" required autocomplete="off"/>
+                                         name="shortened" x-model="shortened" required autocomplete="off">
+                    <!-- Generate shortened -->
+                    <div class="group">
+                        <button x-on:click="generateShortened()" data-tooltip-target="tooltip-right" data-tooltip-placement="right" type="button"
+                                class="text-blue-700 hover:text-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center dark:text-blue-600 dark:hover:text-blue-700 dark:focus:ring-blue-800">
+                            <x-majestic-reload-circle-line/>
+                        </button>
+
+                        <div id="tooltip-right" role="tooltip"
+                             class="absolute z-10 invisible group-hover:visible inline-block px-3 py-2.5 text-sm font-medium text-white bg-gray-800 rounded-lg shadow-sm opacity-0 transition-opacity  group-hover:opacity-100 tooltip dark:bg-gray-600">
+                            Generate
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                        </div>
+                    </div>
+                </x-text-input-left-addon>
                 <x-input-error :messages="$errors->get('shortened')" class="mt-2"/>
             </div>
 
