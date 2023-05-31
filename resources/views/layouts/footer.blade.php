44bb48ab3834c20d222ccc22ddf3eb9352930fb0
Idomo <Idomo@users.noreply.github.com>
2023-05-31 05:33:14 +0300

    Set footer to be fixed bottom

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <div
-    class="w-full bg-white border-t border-gray-200 shadow flex items-center justify-between p-3 dark:bg-gray-800 dark:border-gray-600">
+    class="fixed bottom-0 left-0 z-20 w-full bg-white border-t border-gray-200 shadow flex items-center justify-between p-3 dark:bg-gray-800 dark:border-gray-600">
     <span class="text-sm text-gray-500 mx-auto dark:text-gray-400">
          2023 © Built by <a href="https://idomo.site" class="font-bold hover:underline">Ido Monzon</a>
     </span>
